e4dee1f8e3e3f3cef0f24a268dc4afd7cfc2548c
Juanes1805 <jesteban101805@outlook.com>
2024-08-23 21:10:37 -0500

    Create class Todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,23 @@
 # TODO: Add code here
+import self
+
+
+class Todo:
+    completed: bool = False
+    tags: list[str] = []
+
+    def __int__(self, code_id: int, tittle: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = tittle
+        self.description: str = description
+
+
+
+
+
+
+
+
+
+
+
